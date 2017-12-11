96e5eb49f31268275962120eedb9a37b14081006
hamme503 <hamme503@morris.umn.edu>
2017-12-11 00:41:58 -0600

    implement preliminary solution for summarize_tree_ftw

---
+++ summarize_tree/summarize_tree_ftw.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ftw.h>
+#include <sys/stat.h>
+#include <string.h>
+
+
+#define MAX_FTW_DEPTH 16
+
+static int num_dirs, num_regular;
+
+static int callback(const char *fpath, const struct stat *sb, int typeflag) {
+  if (typeflag == FTW_D) {
+    num_dirs++;
+    return 0;
+  } else {
+    if (typeflag == FTW_F) {
+      num_regular++;
+      return 0;
+    }
+  }
+
+  return 1;
+  
+}
+
+
+int main(int argc, char** argv) {
+
+  if (argc != 2) {
+    printf("Need exactly two arguments\n");
+  }
+
+  num_dirs = 0;
+  num_regular = 0;
+
+  if (ftw(argv[1], callback, MAX_FTW_DEPTH) != 0) {
+    printf("FTW Error\n");
+    return 1;
+  }
+
+  printf("Number of directories: %d\n", num_dirs);
+  printf("Number of files: %d\n", num_regular);
+  
+}
